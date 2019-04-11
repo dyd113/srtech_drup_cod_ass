7ac6443b210017da15c072d0790f1e497bda16fd
deepaky113 <deep3265@gmail.com>
2019-04-12 00:34:39 +0530

    problem 2 opt. code

--- problem2/include/DataProcess.php
+++ problem2/include/DataProcess.php
@@ -1,48 +1,66 @@
 <?php
 
-class DataProcess{
+class DataProcess
+{
+
     private $inputDataS1;
+
     private $inputDataS2;
+
     private $inputDataC;
+
     private $inputDataI;
+
     private $tempOut;
 
-    function __construct() {
+
+    function __construct()
+    {
         $this->inputDataS1 = isset($_POST['inputDataS1']) ? json_decode($_POST['inputDataS1']) : [];
         $this->inputDataS2 = isset($_POST['inputDataS2']) ? json_decode($_POST['inputDataS2']) : [];
-        $this->inputDataC = isset($_POST['inputDataC']) ? json_decode($_POST['inputDataC']) : [];
-        $this->inputDataI = isset($_POST['inputDataI']) ? json_decode($_POST['inputDataI']) : [];
+        $this->inputDataC  = isset($_POST['inputDataC']) ? json_decode($_POST['inputDataC']) : [];
+        $this->inputDataI  = isset($_POST['inputDataI']) ? json_decode($_POST['inputDataI']) : [];
+        $this->tempOut     = [];
+
+    }//end __construct()
+
+
+    function start()
+    {
         $this->tempOut = [];
-    }
-
-    function start() {
-      $this->tempOut = [];
-      for($i=0;$i<count($this->inputDataS1);$i++){
-          $s1 = $this->inputDataS1[$i];
-          $s2 = $this->inputDataS2[$i];
-          $c = $this->inputDataC[$i];
-          $ii =  $this->inputDataI[$i];
-          $curr_outp = 'NO Worries';
-          $str_pos = FALSE;
-          if($c == 'Y' || $c == 'y'){
-            $sub_str = substr($s1, $ii);
-            if (preg_match("~\b".$s2."\b~",$sub_str)){
-              $str_pos = strpos($sub_str, $s2, $ii);
+        $ctnInpS       = count($this->inputDataS1);
+        for ($i = 0; $i < $ctnInpS; $i++) {
+            $s1       = $this->inputDataS1[$i];
+            $s2       = $this->inputDataS2[$i];
+            $c        = $this->inputDataC[$i];
+            $ii       = $this->inputDataI[$i];
+            $currOutp = 'NO Worries';
+            $strPos   = false;
+            if ($c === 'Y' || $c === 'y') {
+                $subStr = substr($s1, $ii);
+                if (preg_match("~\b".$s2."\b~", $subStr)) {
+                    $strPos = strpos($subStr, $s2, $ii);
+                }
+            } else {
+                $strPos = strpos($s1, $s2, $ii);
             }
-          }else{
-            $str_pos = strpos($s1, $s2, $ii);
-          }
-          if($str_pos || $str_pos === 0){
-            $curr_outp = $str_pos;
-          }
-          $this->tempOut[$i] = $curr_outp;
-      }
 
-      echo "<div class='center_div'> <h3>SAMPLE OUT IS :</h3> <br />";
-      for($k=0;$k<count($this->tempOut);$k++){
-        echo $this->tempOut[$k].'<br />';
-      }
-      echo '</div>';
+            if ($strPos || $strPos === 0) {
+                $currOutp = $strPos;
+            }
 
-    }
-}
+            $this->tempOut[$i] = $currOutp;
+        }//end for
+
+        $ctnTemp = count($this->tempOut);
+        echo "<div class='center_div'> <h3>SAMPLE OUT IS :</h3> <br />";
+        for ($k = 0; $k < $ctnTemp; $k++) {
+            echo $this->tempOut[$k].'<br />';
+        }
+
+        echo '</div>';
+
+    }//end start()
+
+
+}//end class
